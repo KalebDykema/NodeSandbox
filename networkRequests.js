5f056aa655d78d69d744a3dbd927257d23fd177d
Kaleb Dykema <dykemakaleb@gmail.com>
2022-04-16 02:24:39 -0500

    Network requests test

---
+++ networkRequests.js
@@ -0,0 +1,21 @@
+const https = require("https");
+const options = {
+   hostname: "https://kaleb.org",
+   port: 443,
+   path: "/posts/1",
+   method: "GET"
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
